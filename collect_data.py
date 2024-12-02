cf576afb791f08a69990c6ab104f97a5ad3af97e
camilajaviera <cjmunozn@hotmail.com>
2024-12-02 19:00:40 -0300

    fix(collect data): fixing the code, it couldn't collect the data appropriately

--- collect_data.py
+++ collect_data.py
@@ -9,38 +9,39 @@ def collect_data():
 
     while True:
         num_temp = input("Enter a temperature (in celsius) value (or type 'exit' to finish): ")
-
-        if num_temp.lower == 'exit': #Exit the loop if the user types the word 'exit'
+      
+        if num_temp.lower() == "exit": #exit the loop if the user types the word 'e'
             break
-        try:
-            num_temp = int(num_temp) #Convert input to an integer
-            temperature.append(num_temp) #Append valir temperature to the list
-        except:
-            print("Invalid input. Please enter numbers only or type 'exit'")
-    
+        else:
+            try:
+                num_temp = int(num_temp) #Convert input to an integer
+                temperature.append(num_temp) #Append valir temperature to the list
+            except:
+                print("Invalid input. Please enter numbers only or type 'exit'")
+        
     #Collect sales data
-    ice_crea_sales = []
-
+    ice_cream_sales = []
+   
     while True:
-        num_sales = input("Enter a sales value (or type 'exit to finish'): ")
+        num_sales = input("Enter a sales value (or type 'e' to finish'): ")
 
-        if num_sales == 'exit':
+        if num_sales.lower() == "exit":
             break
-
-        try:
-            num_sales = int(num_sales)
-            ice_crea_sales.append(num_sales)
-        except:
-            print("Invalid input. Please enter numbers only or type 'exit'")
+        else:
+            try:
+                num_sales = int(num_sales)
+                ice_cream_sales.append(num_sales)
+            except:
+                print("Invalid input. Please enter numbers only or type 'e'")
 
     #Verify that both lists have the same lenght
-    if len(temperature) != len(ice_crea_sales):
+    if len(temperature) != len(ice_cream_sales):
         print("\nThe temperature and sales lists MUST HAVE the same number of elements.")
         print("Please re-enter the data\n")
         return collect_data() #Recursively call the function to restart data collection
     
     #Convert lists to NumPy arrays for further processing
     temp = np.array(temperature).reshape(-1, 1).astype(int) #Reshape to a column vector
-    sales = np.array(ice_crea_sales).reshape(-1, 1).astype(int) #Reshape to a column vector
+    sales = np.array(ice_cream_sales).reshape(-1, 1).astype(int) #Reshape to a column vector
 
     return temp, sales #Return the temperature and sales array
